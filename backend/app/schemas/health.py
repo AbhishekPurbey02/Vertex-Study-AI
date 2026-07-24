cf58cc51cd49b5767e0d3337750554e8b3599c2e
Abhishek Purbey <133017292+AbhishekPurbey02@users.noreply.github.com>
2026-07-24 23:55:51 +0545

    Update and refactored the code of health.py

--- backend/app/schemas/health.py
+++ backend/app/schemas/health.py
@@ -6,3 +6,8 @@ class HealthResponse(BaseModel):
     app: str
     version: str
     environment: str
+
+class DocumentUploadResponse(BaseModel):
+    filename: str
+    content_type: str
+    status: str
