bbaa5cb658c2005d18583ae9370bd651da974477
Abhishek Purbey <133017292+AbhishekPurbey02@users.noreply.github.com>
2026-07-29 22:17:39 +0545

    Update the text_service file

--- backend/app/services/text_service.py
+++ backend/app/services/text_service.py
@@ -1,1 +1,14 @@
 import re
+
+def clean_text(text: str) -> str:
+    text = text.replace("\r", "\n")
+
+    text = re.sub(r"[\t]+", " ", text)
+
+    text = re.sub(r"\n{3,}", "\n\n", text)
+
+    lines = [line.strip() for line in text.split("\n")]
+
+    cleaned_lines = [line for line in lines if line]
+
+    return "\n".join(cleaned_lines)
